799da18819ba158855964c42d8519daeabaf867b
Youzhi Qu <43301543+YouzhiQu@users.noreply.github.com>
2020-09-10 20:24:49 +0800

    update train.py file

--- Train.py
+++ Train.py
@@ -22,16 +22,22 @@ def main():
     else:
         sess.run(tf.initialize_all_variables())
 
-    for i in range(max_iter):
+    for i in range(20000):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = sess.run(lenet.train_accuracy,feed_dict={
                 lenet.raw_input_image: batch[0],lenet.raw_input_label: batch[1]
             })
-            print("step %d, training accuracy %g" % (i, train_accuracy))
+            train_loss = sess.run(lenet.loss, feed_dict={
+                lenet.raw_input_image: batch[0], lenet.raw_input_label: batch[1]
+            })
+            print("step %d, training accuracy %g, training loss %g" % (i, train_accuracy, train_loss))
         sess.run(lenet.train_op,feed_dict={lenet.raw_input_image: batch[0],lenet.raw_input_label: batch[1]})
     save_path = saver.save(sess, parameter_path)
-
+    test_accuracy = sess.run(lenet.train_accuracy, feed_dict={
+        lenet.raw_input_image: mnist.test.images, lenet.raw_input_label: mnist.test.labels
+    })
+    print("test accuracy %g" % (test_accuracy))
 if __name__ == '__main__':
     main()
 
